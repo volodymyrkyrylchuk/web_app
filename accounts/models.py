06efa4e68d81e514aa8f713c23e81d9856a8ea5a
Volodymyr Kyrylchuk <vlk@group-fs.com>
2020-08-04 19:16:32 +0300

    feat: create applications for accounts

---
+++ accounts/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+# Create your models here.
+class Profile(models.Model):
+    id = models.AutoField(primary_key=True)
+    login = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=64)
+    nickname = models.CharField(max_length=124, unique=True, default=login)
+
+    def __str__(self):
+        return f'nickname - {self.nickname}, id - {self.id}'
+
+    def profiles_list(self):
+        pass
+
+    def get_name(self):
+        return self.nickname
+
+
