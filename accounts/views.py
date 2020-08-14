52cec87e5f10776f026daf0f3f9390964504bc8b
Volodymyr Kyrylchuk <vlk@group-fs.com>
2020-08-14 17:47:09 +0300

    feat: add form for ctreating new profile

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,55 @@
+from django.http.response import HttpResponseRedirect
+
+from django.db.models import Q
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from accounts.forms import ProfileAddForm
 from accounts.models import Profile
 
 
 # Create your views here.
 def get_profiles_list(request):
     objects = Profile.objects.all()
-    nick = request.GET.get('nickname')
-    if nick:
-        objects = objects.filter(nickname__icontains=nick, )
+    search = request.GET.get('search1')
+    if search:
+        objects = objects.filter(Q(nickname__icontains=search) | Q(login__icontains=search))
 
-    iterator = (f'{obj.nickname} - {obj.login}' for obj in objects)
-    result = '<br>'.join(iterator)
-    return HttpResponse(result)
+    return render(
+        request,
+        'profiles.html',
+        context={
+            'result': objects,
+            'search': search
+        }
+    )
+
+
+def get_profile(request, id):
+    profile = Profile.objects.get(id=id)
+    return render(
+        request,
+        'profile.html',
+        context={
+            'profile': profile
+        }
+    )
+
+
+def add_profile(request):
+    if request.method == 'POST':
+        form = ProfileAddForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/profiles')
+
+    elif request.method == 'GET':
+        form = ProfileAddForm()
+
+    return render(
+        request,
+        template_name='profile_add.html',
+        context={
+            'form': form
+        }
+    )
+
