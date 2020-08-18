a19566b27e4c760b67e6d1970e8540769c70e525
vladyslavgolovanov <cuzuki1@gmail.com>
2020-08-18 12:07:47 +0300

    feat: create model Publication

--- accounts/models.py
+++ accounts/models.py
@@ -8,7 +8,16 @@ class Profile(models.Model):
     password = models.CharField(max_length=64)
     nickname = models.CharField(max_length=124, unique=True, default=login)
 
+    def get_publication(self):
+        return Publication.objects.filter(profile_id=self.id)
+
     def __str__(self):
         return f'nickname - {self.nickname}, id - {self.id}, password - {self.password}'
+
+class Publication(models.Model):
+    data = models.DateTimeField(auto_now_add=True)
+    profile_id = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    comments = models.CharField(max_length=248,blank=True)
+    contens = models.FileField(null=True, blank=True, upload_to="image")
 
 
