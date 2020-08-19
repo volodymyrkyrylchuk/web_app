61869946f4cef0dca889fd21140c818e321a620d
sergoyats <61284604+sergoyats@users.noreply.github.com>
2020-08-19 16:12:24 +0300

    added a model for publications

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 
-# Create your models here.
 class Profile(models.Model):
     id = models.AutoField(primary_key=True)
     login = models.CharField(max_length=255, unique=True)
