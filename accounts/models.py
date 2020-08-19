8650c6c81347118a606a0009bb142e3f3a82f533
sergoyats <61284604+sergoyats@users.noreply.github.com>
2020-08-19 16:04:04 +0300

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -12,3 +12,11 @@ class Profile(models.Model):
         return f'nickname - {self.nickname}, id - {self.id}, password - {self.password}'
 
 
+class Publication(models.Model):
+    publication_date = models.DateTimeField('published', auto_now=True)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    media = models.ImageField(upload_to='media/', null=False, blank=True)
+    comments = models.CharField(max_length=260, blank=True)
+
+    def __str__(self):
+        return f'comment by {self.author} on {self.publication_date}'
