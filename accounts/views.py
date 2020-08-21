9d66b6eefc4b77dc6721d6cf8a6216ea16652a38
Volodymyr Kyrylchuk <vlk@group-fs.com>
2020-08-21 19:22:25 +0300

    feat: add routes

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,9 @@
-from django.http.response import HttpResponseRedirect
-
+from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q
+from django.http.response import HttpResponseRedirect, HttpResponseNotFound
 from django.shortcuts import render
 
-from accounts.forms import ProfileAddForm
+from accounts.forms import ProfileAddForm, ProfileEditForm
 from accounts.models import Profile
 
 
@@ -24,8 +24,8 @@ def get_profiles_list(request):
     )
 
 
-def get_profile(request, id):
-    profile = Profile.objects.get(id=id)
+def get_profile(request, slug):
+    profile = Profile.objects.get(id=slug)
     return render(
         request,
         'profile.html',
@@ -52,3 +52,27 @@ def add_profile(request):
             'form': form
         }
     )
+
+
+def edit_profile(request, slug):
+    try:
+        profile = Profile.objects.get(id=slug)
+    except ObjectDoesNotExist:
+        return HttpResponseNotFound(f'Profile id {slug} doesnt exist')
+    if request.method == 'POST':
+        form = ProfileEditForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(f'/profiles/show/{slug}')
+
+    elif request.method == 'GET':
+        form = ProfileEditForm(instance=profile)
+
+    return render(
+        request,
+        template_name='profile_edit.html',
+        context={
+            'form': form
+        }
+    )
+
