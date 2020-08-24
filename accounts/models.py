06005147978ac042216ae96b622665362c63916a
OOstrovskyi <paragelmen7@gmail.com>
2020-08-24 17:05:20 +0300

    add publication and comment models

--- accounts/models.py
+++ accounts/models.py
@@ -11,4 +11,16 @@ class Profile(models.Model):
     def __str__(self):
         return f'nickname - {self.nickname}, id - {self.id}, password - {self.password}'
 
+class Publication(models.Model):
+    id = models.AutoField(primary_key=True)
+    create_date = models.DateTimeField(auto_now=False, auto_now_add=True)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    media = models.FileField(upload_to='static/uploads/')
+
+
+class Comment(models.Model):
+    id = models.AutoField(primary_key=True)
+    comment_text = models.CharField(max_length=500)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    publication = models.ForeignKey(Publication, on_delete=models.CASCADE)
 
