9760214b5e72f07c8e2f936ba188b343e93dfea5
sergoyats <61284604+sergoyats@users.noreply.github.com>
2020-09-16 16:50:06 +0300

    added the Сomment

--- accounts/models.py
+++ accounts/models.py
@@ -12,10 +12,20 @@ class Profile(models.Model):
 
 
 class Publication(models.Model):
+    id = models.AutoField(primary_key = True)
     publication_date = models.DateTimeField('published', auto_now=True)
     author = models.ForeignKey(Profile, on_delete=models.CASCADE)
     media = models.ImageField(upload_to='media/', null=False, blank=True)
-    comments = models.CharField(max_length=260, blank=True)
 
     def __str__(self):
-        return f'comment by {self.author} on {self.publication_date}'
+        return f'published by {self.author} on {self.publication_date}'
+
+
+class Comment(models.Model):
+    id = models.AutoField(primary_key = True)
+    comment_text = models.CharField(max_length=250, default=None)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    publication = models.ForeignKey(Publication, related_name='comments', on_delete=models.CASCADE)
+
+def __str__(self):
+        return f'commented by {self.author}'
